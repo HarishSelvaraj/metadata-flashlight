716616869dc922129e1a13c5109e74e59fcd6d11
Rajeswari N <rajeswari.n@tvsnext.io>
2018-07-02 17:42:45 +0530

    added label and textarea componenet

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -28,6 +28,8 @@ import { SearchDocumentComponent } from './search-document/search-document.compo
 import { ComponentLoaderDirective } from '../directives/component-loader.directive';
 import { DatePickerComponent } from './date-picker/date-picker.component';
 import { ButtonComponent } from './button/button.component';
+import { LabelComponent } from './label/label.component';
+import { TextareaComponent } from './textarea/textarea.component';
 
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
@@ -48,7 +50,9 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     ComponentLoaderDirective,
     DatePickerComponent,
     ButtonComponent,
-    DialogOverviewExampleDialog
+    DialogOverviewExampleDialog,
+    LabelComponent,
+    TextareaComponent
   ],
 
   imports: [
@@ -83,7 +87,8 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     SearchLayoutComponent,
     ComponentLoaderDirective
   ],
-  entryComponents: [DialogOverviewExampleDialog,SearchLayoutComponent, InputTextComponent, SelectComponent, DatePickerComponent, ButtonComponent],
+  entryComponents: [DialogOverviewExampleDialog,SearchLayoutComponent, InputTextComponent, 
+    SelectComponent, DatePickerComponent, ButtonComponent, LabelComponent, TextareaComponent],
   providers: [
     {
       provide: PERFECT_SCROLLBAR_CONFIG,
